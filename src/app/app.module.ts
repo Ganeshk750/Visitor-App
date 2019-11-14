424220da3ce2a9414978d784b11785bf21b5865a
Ganeshk750 <ganesh9ot@gmail.com>
2019-11-14 19:03:47 +0530

    adding set ups and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './shared/header.component';
+import { FooterComponent } from './shared/footer.component';
+import { LoginComponent } from './shared/login.component';
+import { SignupComponent } from './shared/signup.component';
+import { AboutusComponent } from './shared/aboutus.component';
+import { RegisterResultComponent } from './register/register-result.component';
+import { HostAddComponent } from './host/host-add.component';
+import { HostEditComponent } from './host/host-edit.component';
+import { HostResultComponent } from './host/host-result.component';
+import { HostSearchComponent } from './host/host-search.component';
+import { GuestAddComponent } from './guest/guest-add.component';
+import { GuestEditComponent } from './guest/guest-edit.component';
+import { GuestResultComponent } from './guest/guest-result.component';
+import { GuestSearchComponent } from './guest/guest-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    SignupComponent,
+    AboutusComponent,
+    RegisterResultComponent,
+    HostAddComponent,
+    HostEditComponent,
+    HostResultComponent,
+    HostSearchComponent,
+    GuestAddComponent,
+    GuestEditComponent,
+    GuestResultComponent,
+    GuestSearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
